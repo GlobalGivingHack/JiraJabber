273fc7de599a94c19b47581e2b61cbe17f561936
Joe McGinty <joe.mcginty@hivehome.com>
2017-11-30 12:11:31 -0800

    jira integration update

--- jiraIntegration/jiraIntegration/Ticket.py
+++ jiraIntegration/jiraIntegration/Ticket.py
@@ -1,4 +1,3 @@
-import os
 import requests
 import json
 from requests.auth import HTTPBasicAuth
@@ -22,7 +21,37 @@ class Ticket(BaseClass):
         self.jira_auth = self.get_jira_auth()
         self.ticket_url = None
         super(Ticket, self).__init__(is_lambda)
+        if 'key' in data:
+            self._load_ticket_from_key(data['key'])
         self.set_ticket_data()
+    
+    def _load_ticket_from_key(self, key):
+        try:
+
+            find_ticket_url = '{0}/rest/api/2/issue/{1}'.format(self.CONFIGS['jira_url'], key)
+            headers = {'Content-Type': 'application/json'}
+            request = requests.get(find_ticket_url, headers=headers, auth=self.jira_auth)
+            if request.status_code == 200:
+                self.load_ticket(request.json())
+        except Exception as e:
+            super(Ticket, self).print_error('Error occurred while creating ticket', e)
+        
+    def load_ticket(self, ticket_data):
+
+        self.key = ticket_data['key']
+        self.ticket_type = ticket_data['fields']['issuetype']
+        self.fixVersions = ticket_data['fields']['fixVersions']
+        self.created = ticket_data['fields']['created']
+        self.priority = ticket_data['fields']['priority']
+        self.labels = ticket_data['fields']['labels']
+        self.assignee = ticket_data['fields']['assignee']
+        self.updated = ticket_data['fields']['updated']
+        self.status = ticket_data['fields']['status']
+        self.description = ticket_data['fields']['description']
+        self.creator = ticket_data['fields']['creator']
+        self.project = ticket_data['fields']['project']
+        self.subject = ticket_data['fields']['summary']
+
 
     def set_ticket_data(self):
         self.update_ticket(self.get_project_field())
@@ -39,10 +68,43 @@ class Ticket(BaseClass):
             request = requests.post(create_ticket_url, headers=headers, auth=self.jira_auth,
                                     data=json.dumps(self.ticket_data))
             # Printing below will help determine why ticket creation failed. e.g. "custom_fields" your ticket requires
-            print request.text
-            self.ticket_url = request.json()['key']
+            self._load_ticket_from_key(request.json()['key'])
         except Exception as e:
             super(Ticket, self).print_error('Error occurred while creating ticket', e)
+
+    def update(self):
+        pass
+        #todo
+
+    def move_to_sprint(self, sprint_id):
+        try:
+            headers = {'Content-Type': 'application/json'}
+            create_ticket_url = '{0}/rest/api/2/issue/{1}'.format(self.CONFIGS['jira_url'], self.key)
+
+            body = {"update": {"customfield_10010": [{"set": sprint_id}]}}
+            request = requests.put(create_ticket_url, headers=headers, auth=self.jira_auth,
+                                    data=json.dumps(body))
+            # Printing below will help determine why ticket creation failed. e.g. "custom_fields" your ticket requires
+            self.load_ticket(request.json())
+        except Exception as e:
+            super(Ticket, self).print_error('Error occurred while creating ticket', e)
+
+    def change_status(self, status):
+        try:
+
+            headers = {'Content-Type': 'application/json'}
+
+            status_change_ticket_url = '{0}/rest/api/2/issue/{1}/transitions'.format(self.CONFIGS['jira_url'], self.key)
+
+            body = {"transition": {"id": "21"}}
+            request = requests.post(status_change_ticket_url, headers=headers, auth=self.jira_auth,
+                                   data=json.dumps(body))
+            print request
+            # Printing below will help determine why ticket creation failed. e.g. "custom_fields" your ticket requires
+            self.load_ticket(request.json())
+        except Exception as e:
+            super(Ticket, self).print_error('Error occurred while creating ticket', e)
+
 
     def set_custom_fields(self):
         try:
@@ -54,7 +116,13 @@ class Ticket(BaseClass):
             super(Ticket, self).print_error('Could not set custom fields. Please refer to documentation', e)
 
     def get_issuetype_field(self):
-        return {self.ISSUETYPE_KEY: {'name': self.data.get(self.ISSUETYPE_KEY, self.CONFIGS['issuetype'])}}
+        issue_type = self.data.get(self.ISSUETYPE_KEY, self.CONFIGS['issuetype'])
+        if issue_type.lower() == "bug":
+            issue_type = "Bug"
+        elif issue_type.lower() == "story":
+            issue_type = "Story"
+
+        return {self.ISSUETYPE_KEY: {'name': issue_type}}
 
     def get_project_field(self):
         return {self.PROJECT_KEY: {'key': self.CONFIGS['jira_project']}}
@@ -79,3 +147,12 @@ class Ticket(BaseClass):
     def get_jira_auth(_self):
         return HTTPBasicAuth(_self.CONFIGS['jira_username'], _self.CONFIGS['jira_password'])
 
+    def to_hash(self):
+        return {
+            'key': self.key,
+            'type': self.ticket_type['name'],
+            'project': self.project['key'],
+            'description': self.description,
+            'subject': self.summary,
+            'priority': self.priority['name']
+        }
--- jiraIntegration/jiraIntegration/alertToTicket.py
+++ jiraIntegration/jiraIntegration/alertToTicket.py
@@ -1,16 +1,38 @@
 from Ticket import Ticket
-from PagerDuty import PagerDuty
-
 
 def lambda_handler(event, context):
-    main(event, is_lambda=True)
+    return main(event, is_lambda=True)
 
 def main(event, is_lambda=True):
     print 'in main for event {0}'.format(event)
+    if event['action'] == 'create':
+        return create(event['data'], is_lambda)
+    elif event['action'] == 'update':
+        pass
+        #return update(event['data'], is_lambda)
+    elif event['action'] == 'read':
+        return read(event['data'], is_lambda)
+    elif event['action'] == 'status_change':
+        return status_change(event['data'], is_lambda)
+    else:
+        return False
+
+def read(event, is_lambda):
+    ticket = Ticket(event, is_lambda)
+    return ticket.to_hash()
+
+def status_change(event, is_lambda):
+    ticket = Ticket(event, is_lambda)
+    ticket.change_status("status")
+    return ticket.to_hash()
+
+def create(event, is_lambda):
     try:
         ticket = Ticket(event, is_lambda)
         print 'Creating new ticket'
         ticket.create()
+        print 'created ticket with data: {0}'.format(vars(ticket))
+        return ticket.to_hash()
         # TODO update_pager_duty_with_ticket_info()
     except Exception as e:
         print 'Error: {0}'.format(e)
@@ -18,6 +40,9 @@ def main(event, is_lambda=True):
 
 if __name__ == '__main__':
     event = {
-        "subject": "45645"
+        'action': 'create',
+        'data': {
+            "subject": "45645"
+        }
     }
     main(event, is_lambda=False)
